d618328847a045f0efc5d0c39294238011e98d85
dl7255971-debug <dl7255971-debug@users.noreply.github.com>
2026-04-25 18:01:03 -0300

    chore: use environment variables for supabase config

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://usvqeaejllrfnnvubnbz.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVzdnFlYWVqbGxyZm5udnVibmJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcxMzc0MzYsImV4cCI6MjA5MjcxMzQzNn0.H_8Ca_XzBTg6vPFD7vTkCpzuH_mdIyoVyL1C11tAhPs'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
